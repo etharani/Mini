7a7e5c7b8fa17c880b221be1c23cca2eaec92f05
etharani <tharani2297@gmail.com>
2023-12-19 07:38:57 +0530

    add

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const port = 8080;
+const user = require("./routes/users");
+const school=require("./routes/school")
+
+app.use(express.json());
+app.use(cors());
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://tharani2297:tharu%404411@tharu.jikk6hd.mongodb.net/education')
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error', err);
+  });
+
+app.use(user);
+app.use(school);
+
+app.get("/", (req, res) => {
+  res.json("hi");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ education/src/pages/school/School.jsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+import "./School.css";
+import NavBar from "../../components/navBar/NavBar";
+import Footer from "../../components/footer/Footer";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+
+const School = (props) => {
+
+  const ids=props.ids;
+  const { id } = useParams();
+  const [values, setValues] = useState([]);
+  const navigate=useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(`http://localhost:8080/school/${id}`);
+        setValues(result.data);
+        console.log(values._id);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [1]);
+
+  const deleteDetail = async (id) => {
+    try {
+      const details = await axios.delete(
+        `http://localhost:8080/volunteer/${id}`
+      );
+      console.log(details.data);
+      navigate("/volunteer")
+     
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
+  return (
+    <div className="topschoolcontainers">
+      <NavBar />
+      <div className="upBody">
+        <div className="firstcontent">
+          <h4>Hello, welcome to College</h4>
+          <span>
+            This is your profile page. You can see the progress and also you can
+            edit your details.
+          </span>
+          <img
+            src={values.verification}
+          />
+        </div>
+        <div className="secondcontent">
+          <div className="input">
+            <label htmlFor="schoolname">School name</label>
+            <input type="text" id="schoolname" name="schoolname" value={values.schoolname} />
+          </div>
+          <div className="input">
+            <label htmlFor="address">Address</label>
+            <input type="text" id="address" name="address" value={values.address} />
+          </div>
+          <div className="input">
+            <label htmlFor="state">State or  province</label>
+            <input type="text" id="state" name="state" value={values.state} />
+          </div>
+          <div className="input">
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" name="email" value={values.email} />
+          </div>
+        </div>
+        <div className="thirdcontent">
+        <div className="input">
+            <label htmlFor="subject">Need subject</label>
+            <input type="text" id="subject" name="subject" value={values.subject} />
+          </div>
+          <div className="input">
+            <label htmlFor="city">City</label>
+            <input type="text" id="city" name="city" value={values.city} />
+          </div>
+        
+          <div className="input">
+            <label htmlFor="phoneno">Phone no</label>
+            <input type="text" id="phoneno" name="phoneno" value={values.phoneno} />
+          </div>
+        </div>
+      </div>
+      <div className="downBody">
+        <span hidden>Requirements:</span>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        {ids ? (
+  <Link to={`/school/update/${values._id}`} className="btn1" >Update</Link>
+) : (
+ 
+  <Link to={`/school/update/${values._id}`} className="btn1" hidden>Update</Link>
+)}
+ {console.log("ids",ids)}
+       
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default School;
